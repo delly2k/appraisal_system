61f275a4edfd0605d7007416ddcef8baa536482c
DBJMIS <mis@dbankjm.com>
2026-03-19 14:59:28 -0500

    PDF 2

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   outputFileTracingRoot: path.join(__dirname),
-  serverExternalPackages: ["next-auth"],
+  serverExternalPackages: ["next-auth", "@sparticuz/chromium", "puppeteer-core"],
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/@sparticuz/chromium/**",
+      "./node_modules/puppeteer-core/**",
+    ],
+  },
 };
 
 module.exports = nextConfig;
